786a73836b159959f6637ea4de5f82290c150aa1
Smokashi23 <sakshimokashi640@gmail.com>
2024-02-19 17:10:00 +0530

    assignmemt3 added

---
+++ Q1.js
@@ -0,0 +1,9 @@
+let age = 25;
+
+if (age <= 12) {
+    console.log("Child");
+} else if (age >= 13 && age <= 18) {
+    console.log("Teen");
+} else {
+    console.log("Adult");
+}
---
+++ Q4.js
@@ -0,0 +1,10 @@
+let arraySize = 25;
+let Arr = [];
+let i = 1;
+
+while (i <= arraySize) {
+    Arr.push(i);
+    i++;
+}
+
+console.log(Arr); 
