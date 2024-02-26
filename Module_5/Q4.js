a488c4cffd523f83ee206d44b9d5bb1786f19317
Smokashi23 <sakshimokashi640@gmail.com>
2024-02-26 16:11:04 +0530

    modified

--- Module_5/Q4.js
+++ Module_5/Q4.js
@@ -1,4 +1,16 @@
 const JsonString = '{"name": "sakshi", "age": 21, "city": "Solapur"}';
 const parse = JSON.parse(JsonString);
 
-console.log(parse);
+
+//Json passing dirty string
+/*
+const jsonString = '{"name": "sakshi", "age": 21, "city": "Solapur",}';
+
+try {
+  const parse = JSON.parse(jsonString);
+  console.log(parse);
+} catch (error) {
+  console.error("Error parsing JSON:", error.message);
+}
+*/
+
